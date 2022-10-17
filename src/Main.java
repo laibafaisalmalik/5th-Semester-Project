cab67b8acb89223b5a1c7b315d26e297fccafb0a
laibafaisalmalik <laibam45@gmail.com>
2022-10-18 00:46:47 +0500

    First Version

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author laiba
+ */
+public class Main {
+    public static void main(String [] arg){
+        
+        Login l=new Login();
+        l.setVisible(true);
+        l.setLocationRelativeTo(null);
+    }
+}
