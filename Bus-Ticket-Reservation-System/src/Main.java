d3c24c8d4d4f217b95b98b844d68218f75cdc918
laibafaisalmalik <laibam45@gmail.com>
2022-10-31 22:12:42 +0500

    Second Version, Project Folder

---
+++ Bus-Ticket-Reservation-System/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author laiba
+ */
+public class Main {
+    public static void main(String [] arg){
+        
+        WelcomePage w = new WelcomePage();
+        w.setVisible(true);
+        w.setLocationRelativeTo(null);
+    }
+}
